d6c7ae401694dfe37b802b07cd099e7d9b56eac0
juayala <143744588+juayala@users.noreply.github.com>
2025-04-23 01:45:33 +0000

    data exported to Dbeaver correctly

--- notebooks/[Source]_API_Extract_Load_Raw.ipynb/.py
+++ notebooks/[Source]_API_Extract_Load_Raw.ipynb/.py
@@ -4,13 +4,25 @@ import pandas as pd
 import html
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 # Load environment variables
 load_dotenv()
 
 # Get the API key 
 api_key = os.getenv('API_KEY')
+# %%
+# Postgres databse connection details 
+pg_user = os.environ['PG_USER']
+pg_password = os.environ['PG_PASSWORD']
+pg_host = os.environ['PG_HOST']
+pg_db = os.environ['PG_DB']
 
+# %%
+pg_conn_str = f'postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_db}'
+pg_engine = create_engine(pg_conn_str)
+
+# %%
 # Test the API key
 handle_name = "boilerroom"
 search_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q={handle_name}&key={api_key}"
@@ -36,14 +48,14 @@ video_response = requests.get(videos_url).json()
 video_info = []
 
 for item in video_response.get('items', []):
-    video_id = item['id'].get('videoId')
-    title = html.unescape(item['snippet']['title'])  # Decode "&amp;" etc.
-    published = item['snippet']['publishedAt']
-    video_info.append({
-        'Video ID': video_id,
-        'Video Title': title,
-        'Published Date': published
-    })
+        video_id = item['id'].get('videoId')
+        title = html.unescape(item['snippet']['title'])  # Decode "&amp;" etc.
+        published = item['snippet']['publishedAt']
+        video_info.append({
+            'Video ID': video_id,
+            'Video Title': title,
+            'Published Date': published
+        })
 
 # Convert to DataFrame
 df = pd.DataFrame(video_info)
@@ -54,12 +66,19 @@ df['Published Date'] = pd.to_datetime(df['Published Date']).dt.strftime('%Y-%m-%
 # Sort by newest first
 df = df.sort_values(by='Published Date', ascending=False).reset_index(drop=True)
 
-# Display settings
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_rows', None)
+# Adjust Pandas display options
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.max_rows', None)     # Show all rows
+pd.set_option('display.max_colwidth', None) # Do not truncate column content
+pd.set_option('display.width', 1000)        # Set the display width to avoid wrapping
 
 # Output cleaned DataFrame
 print(df)
 
+# %%
+# Write DataFrame to videos table in Postgres (raw schema)
+df.to_sql('videos', pg_engine, schema='raw', if_exists='replace', index=False)
 
 # %%
+# Print confirmation message
+print(f'{len(df)} records loaded into Postgres videos table.')
